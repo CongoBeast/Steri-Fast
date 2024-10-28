19b87f85b7ff4cb3da8cc29c291078a8a047b657
Methembe Tshuma <53082789+CongoBeast@users.noreply.github.com>
2024-10-28 08:25:13 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import ManageRequest from "./components/ManageReq";
 import MainNav from './components/MainNav';
 import AboutPage from './components/AboutPage';
 import ToolManagement from './components/ToolManagement';
+import UserManagement from './components/UserManagement';
 import UnsterilizedPackages from './components/UnsterilizedPackages'
 import ProtectedRoute from './components/ProtectedRoute';
 
@@ -24,21 +25,22 @@ function App() {
           <Routes>
             <Route path="/auth" element={<AuthPage />} />
             <Route path="/about" element={<AboutPage />} />
-            <Route path="/ManageRequest" element={<ManageRequest />} />
+            {/* <Route path="/ManageRequest" element={<ManageRequest />} /> */}
             
             <Route element={<ProtectedRoute />}>
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/user-dashboard" element={<UserDashboard />} />
               <Route path="/tool-management" element={<ToolManagement />} />
               <Route path="/unsterilized-packages" element={<UnsterilizedPackages />} />
-              <Route path="/unsterilized-packages" element={<UnsterilizedPackages />} />
+              <Route path="/manage-users" element={<UserManagement />} />
+              <Route path="/ManageRequest" element={<ManageRequest />} />
             </Route>
 
-            <Route path="/dashboard" element={<Dashboard />} />
+            {/* <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/user-dashboard" element={<UserDashboard />} />
             <Route path="/tool-management" element={<ToolManagement />} />
             <Route path="/unsterilized-packages" element={<UnsterilizedPackages />} />
-            <Route path="/unsterilized-packages" element={<UnsterilizedPackages />} />
+            <Route path="/unsterilized-packages" element={<UnsterilizedPackages />} /> */}
 
           </Routes>
         </div>
