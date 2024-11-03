c905012e28aa6f6b223c485c5f34b0b13b16dc18
Methembe Tshuma <53082789+CongoBeast@users.noreply.github.com>
2024-11-03 23:51:56 +0800

    Add files via upload

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,22 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import Breadcrumbs from './Breadcrumbs';
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import MetricsCards from './MetricsCards';
 
-
-
-// const Dashboard = ({ userType, username }) => {
-
-//   const location = useLocation();
-//   const navigate = useNavigate();
-
-//   const handleNavClick = (path) => {
-//     navigate(path);
-//   };
-
-//   const breadcrumbItems = [
-//     { name: 'Home', link: '/', active: false },
-//     { name: 'Inventory', link: '/inventory', active: true },
-//   ];
 
 function Dashboard() {
 
@@ -29,43 +15,51 @@ function Dashboard() {
   };
 
 
+  const metrics = [
+    {value: 45 , label: "Damaged tools"},
+    {value: 37 , label: "Unsterilized packs"},
+    {value: 28 , label: "Requests"},
+    {value: 45 , label: "Users"}
+  ]
+
+
   return (
 
 
-    <div className="container vh-100 d-flex flex-column justify-content-center  align-items-center">
+    <div className="container vh-100 d-flex flex-column  mt-5 align-items-center">
     {/* // <div className="home-container"> */}
 
       <div className="text-center mb-5">
-        <h1>Welcome {localStorage.getItem('user')}</h1>
+        <h1 className="display-4">Welcome {localStorage.getItem('user')}</h1>
       </div>
 
+      <MetricsCards metrics={metrics} className="mb-10" />
+
       <div className="row text-center">
-        <div className="col-md-6 mb-3">
-          <button className="btn btn-primary w-100 py-3 btn-md" onClick={() => handleNavClick('/ManageRequestAll')}>
+        <div className="col-md-3 mb-3">
+          <button className="btn btn-primary w-100" onClick={() => handleNavClick('/ManageRequestAll')}>
             Manage Requests
           </button>
         </div>
-        <div className="col-md-6 mb-3">
-          <button className="btn btn-warning w-100 py-3 btn-md" onClick={() => handleNavClick("/unsterilized-packages")}>
+
+        <div className="col-md-3 mb-3">
+          <button className="btn btn-warning w-100" onClick={() => handleNavClick("/unsterilized-packages")}>
             Manage Packages
           </button>
         </div>
-      </div>
 
-      <div className="row text-center">
-        <div className="col-md-6 mb-3">
-          <button className="btn btn-success w-100 py-3 btn-md"  onClick={() => handleNavClick('/tool-management')}>
-            Tool Management
+        <div className="col-md-3 mb-3">
+          <button className="btn btn-success w-100"  onClick={() => handleNavClick('/tool-management')}>
+            Manage Tools
           </button>
         </div>
 
-        <div className="col-md-6 mb-3">
-          <button className="btn btn-dark w-100 py-3 btn-md" onClick={() => handleNavClick("/manage-users")}>
+        <div className="col-md-3 mb-3">
+          <button className="btn btn-dark w-100" onClick={() => handleNavClick("/manage-users")}>
             Manage Users
           </button>
-          
         </div>
-
+        
       </div>
 
     </div>
